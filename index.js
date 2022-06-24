1f34dc9ea66a28a0ba5ebc0d29929be50d52d09b
Maria1808 <mvbannikova1808@gmail.com>
2022-06-25 00:30:03 +0500

    make it better

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.all('*', function(req, res, next) {
+    var origin = req.get('origin'); 
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
 app.use(cors())
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
