5c85a71888bdf9862808732f34f18fb9ef04f761
Maria1808 <mvbannikova1808@gmail.com>
2022-06-24 22:11:05 +0500

    make it better

--- index.js
+++ index.js
@@ -16,10 +16,9 @@ app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use('/api', router)
-app.get('/', (req, res)=>{
-    res.status(200).json({message:"work"})
-})
-
+app.get('/allow-cors', function(request, response) {
+    response.set('Access-Control-Allow-Origin', '*');
+  });
 
 
 const start = async () => {
