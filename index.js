473837506077fd1918ad1b3ba5d36be2f315f4bf
Maria1808 <mvbannikova1808@gmail.com>
2022-06-24 17:10:03 +0500

    make it better

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use('/api', router)
-app.post('/', (req, res)=>{
+app.get('/', (req, res)=>{
     res.status(200).json({message:"work"})
 })
 
