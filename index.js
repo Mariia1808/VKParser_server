53ae825415f450de2cbd439faedfce30b01ec1b3
Maria1808 <mvbannikova1808@gmail.com>
2022-06-24 21:48:24 +0500

    make it better

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 const app = express()
-app.use(cors({ origin: "https://parservkontakte.netlify.app", credentials: true }))
+app.use(cors())
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
 app.use(express.static(path.resolve(__dirname, 'static')))
