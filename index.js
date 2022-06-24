4477692ad11b1f0684cedb201fe21daa69235577
Maria1808 <mvbannikova1808@gmail.com>
2022-06-24 17:06:09 +0500

    make it better

--- index.js
+++ index.js
@@ -11,14 +11,29 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 const app = express()
-app.use(cors())
+// app.use(cors())
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use('/api', router)
-app.post('/', (req, res)=>{
+app.get('/', (req, res)=>{
     res.status(200).json({message:"work"})
 })
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+  })
 
 
 const start = async () => {
