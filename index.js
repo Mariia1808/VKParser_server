6e1ee7b44be3b120c0cfbfa6321312eb71ab4011
Maria1808 <mvbannikova1808@gmail.com>
2022-06-25 00:33:56 +0500

    make it better

--- index.js
+++ index.js
@@ -11,16 +11,8 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 const app = express()
-app.all('*', function(req, res, next) {
-    var origin = req.get('origin'); 
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-})
 app.use(cors())
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
+app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use('/api', router)
 app.get('/', (req, res)=>{
