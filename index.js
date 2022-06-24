86c108bf91b586f85cb878a3e6b016890e4bfcc0
Maria1808 <mvbannikova1808@gmail.com>
2022-06-25 00:41:47 +0500

    make it better

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-const axios = require('axios')
 const sequelize = require('./db')
-const models = require('./models/models')
 const cors = require('cors')
 const router = require('./routers/index')
 const path = require('path')
@@ -12,10 +10,11 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors())
-app.use(express.json())
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use('/api', router)
-app.get('/', (req, res)=>{
+app.post('/', (req, res)=>{
     res.status(200).json({message:"work"})
 })
 
