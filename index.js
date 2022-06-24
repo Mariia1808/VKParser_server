3d39c1d6d2707034e4823cce5d239eb06b81bb7f
Maria1808 <mvbannikova1808@gmail.com>
2022-06-25 00:26:06 +0500

    make it better

--- index.js
+++ index.js
@@ -11,21 +11,6 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 const app = express()
-var allowCrossDomain = function(req, res, next) {
-    res.headers('Access-Control-Allow-Origin', '*');
-    res.headers('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.headers('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
-
-app.use(allowCrossDomain);
 app.use(cors())
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
