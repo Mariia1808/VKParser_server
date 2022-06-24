a2ea4f9968ec09b5c285f00dbcf6e641b11e0daf
Maria1808 <mvbannikova1808@gmail.com>
2022-06-24 22:42:35 +0500

    make it better

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 const app = express()
-app.use(cors())
+const isProduction = process.env.NODE_ENV === 'production'
+const origin = {
+  origin: isProduction ? 'https://www.example.com' : '*',
+}
+
+app.use(cors(origin))
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
 app.use(express.static(path.resolve(__dirname, 'static')))
