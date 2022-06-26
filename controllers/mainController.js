e2aadee0b61a207115600809cc3d1d51efd2cab0
Maria1808 <mvbannikova1808@gmail.com>
2022-06-26 18:25:43 +0500

    make it better

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -5,8 +5,8 @@ const { History, User, Methods, Parameter } = require('../models/models');
 class MainController { 
 
     async createMethods(req, res){
-        const {name, method0, method1} = req.params
-        const methods = await Methods.create({name:name, method:'\\'+method0 +'\\'+method1})
+        const {name, method} = req.params
+        const methods = await Methods.create({name:name, method:method})
         return res.json({"response":"no_error"})        
     }
 
