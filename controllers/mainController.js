b7b2f7aeea24e4fd6fa4c5f82d9fa850cea8ddca
Maria1808 <mvbannikova1808@gmail.com>
2022-06-26 20:25:42 +0500

    make it better

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -4,12 +4,6 @@ const { History, User, Methods, Parameter } = require('../models/models');
 
 class MainController { 
 
-    async deleteHistory(req, res){
-        //let i = await Parameter.truncate()
-        let methods = await History.drop()
-        return res.json({"response":"no_error"})        
-    }
-
     async createMethods(req, res){
         const {name} = req.params
         const {method} = req.body
@@ -26,22 +20,23 @@ class MainController {
         const {name, id, method, parameters_value} = req.params
         console.log(parameters_value)
         const {itog} = req.body
-        //const history = await History.create({itog:itog, zapros:name, userID:id})
-        let his_param = await Parameter.create({parameters:parameters_value, methodID:method, historyId: (await History.create({itog:itog, zapros:name, userID:id})).id})
+        const history = await History.create({itog:itog, zapros:name, userID:id})
+        let his_param = Parameter.create({parameters:parameters_value, methodID:method, historyId: history.id})
         return res.json({"response":"no_error"})
     }
     async get(req, res){
         const {id} = req.params
-        //const user = await User.findOne({where:{ user_id: id}})
-        let history_user = await History.findAndCountAll({where:{ userID: (await User.findOne({where:{ user_id: id}})).ID}})
+        const user = await User.findOne({where:{ user_id: id}})
+        const history_user = await History.findAndCountAll({where:{ userID: user.ID}})
+        console.log(history_user.rows)
         if(history_user.count!==0){
-            let arr=[]
-            let result = []
-            let parameter = []
+            const arr=[]
+            const result = []
+            const parameter = []
             for (let i in history_user.rows){
-                let param = Parameter.findOne({where:{historyId:history_user.rows[i].id}})
+                const param = await Parameter.findOne({where:{historyId:history_user.rows[i].id}})
                 parameter.push(param)
-                let {dataValues} = Methods.findOne({where:{ ID:param.methodID}})
+                const {dataValues} = await Methods.findOne({where:{ ID:param.methodID}})
                 arr.push(dataValues)
             }
             //console.log(arr)
