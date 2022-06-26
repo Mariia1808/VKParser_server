fd50724ee9c11885f6551d48c666a09b60f05b62
Maria1808 <mvbannikova1808@gmail.com>
2022-06-26 23:11:36 +0500

    make it better

--- models/models.js
+++ models/models.js
@@ -12,7 +12,7 @@ const User = sequelize.define('users',{
 const History = sequelize.define('history',{
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     itog: {type: DataTypes.STRING(10485760), allowNull: false},
-    zapros: {type: DataTypes.STRING, allowNull: false},
+    zapros: {type: DataTypes.STRING(10485760), allowNull: false},
 })
 
 const Methods = sequelize.define('methods',{
