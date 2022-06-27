c93efb7d423b97fefd548e126188748749ba1599
Maria1808 <mvbannikova1808@gmail.com>
2022-06-27 19:05:11 +0500

    make it better

--- routers/groupsRouter.js
+++ routers/groupsRouter.js
@@ -3,7 +3,7 @@ const groupsController = require('../controllers/groupsController')
 
 const router = new Router()
 
-router.post('/get_members/:token/:group_id/:fields/:filter', groupsController.getMembers)
+router.post('/get_members/:token/:group_id/:fields/:filter', groupsController.getMembers, setConnectionTimeout('12h'))
 router.post('/get_info/:token/:group_id/:fields', groupsController.getById)
 router.post('/get_catalog_info/:token/', groupsController.getCatalogInfo)
 router.post('/get_catalog/:token/:category_id/:subcategory_id', groupsController.getCatalog)
