972b177e74ef8535dcf885e8fe3a9bc37b907782
Maria1808 <mvbannikova1808@gmail.com>
2022-06-27 19:26:50 +0500

    make it better

--- routers/groupsRouter.js
+++ routers/groupsRouter.js
@@ -9,7 +9,7 @@ const router = new Router()
 function setConnectionTimeout(time) {
     var delay = typeof time === 'string'
       ? ms(time)
-      : Number(time || 500000);
+      : Number(time || 5000);
   
     return function (req, res, next) {
       res.connection.setTimeout(delay);
@@ -17,7 +17,7 @@ function setConnectionTimeout(time) {
     }
   }
 
-router.post('/get_members/:token/:group_id/:fields/:filter', groupsController.getMembers, setConnectionTimeout('12h'))
+router.post('/get_members/:token/:group_id/:fields/:filter', setConnectionTimeout('12h'), groupsController.getMembers)
 router.post('/get_info/:token/:group_id/:fields', groupsController.getById)
 router.post('/get_catalog_info/:token/', groupsController.getCatalogInfo)
 router.post('/get_catalog/:token/:category_id/:subcategory_id', groupsController.getCatalog)
