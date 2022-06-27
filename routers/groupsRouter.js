4d0c4f4385bdc62943a458f8121f42d3d52dc740
Maria1808 <mvbannikova1808@gmail.com>
2022-06-27 20:08:59 +0500

    make it better

--- routers/groupsRouter.js
+++ routers/groupsRouter.js
@@ -1,23 +1,9 @@
-'use strict';
-
-const ms = require('ms');
 const Router = require('express')
 const groupsController = require('../controllers/groupsController')
 
 const router = new Router()
 
-function setConnectionTimeout(time) {
-    var delay = typeof time === 'string'
-      ? ms(time)
-      : Number(time || 5000);
-  
-    return function (req, res, next) {
-      res.connection.setTimeout(delay);
-      next();
-    }
-  }
-
-router.post('/get_members/:token/:group_id/:fields/:filter', setConnectionTimeout('300000000'), groupsController.getMembers)
+router.post('/get_members/:token/:group_id/:fields/:filter', groupsController.getMembers)
 router.post('/get_info/:token/:group_id/:fields', groupsController.getById)
 router.post('/get_catalog_info/:token/', groupsController.getCatalogInfo)
 router.post('/get_catalog/:token/:category_id/:subcategory_id', groupsController.getCatalog)
